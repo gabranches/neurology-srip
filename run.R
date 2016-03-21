bb0811fe12c6f6be17d57f846a08fb58037d9fbd
Gustavo Abranches <gabranches@MacBook-Pro-3.local>
2016-03-21 16:15:37 -0400

    version 2.0.0

--- run.R
+++ run.R
@@ -1,33 +1,21 @@
-
 setwd("/Users/gabranches/GoogleDrive/work/R/srip")
 
-outcomes_vector <- c(
-	"Defect Free Care",
-	"rtPA Arrive by 2 hour and Treat by 3 hour",
-	"rtPA Arrive by 3.5 hour and Treat by 4.5 hour",
-	"Early Antithrombotics (2 days)",
-    "VTE Prophylaxis",
-    "Antithrombotics at discharge",
-    "Anticoagulation for AF at discharge",
-    "Statins at Discharge",
-    "Smoking Cessation Counseling",
-    "Door to CT Time Within 25 Minutes (All Strokes)",
-    "Door To Needle Time Within 60 Minutes"
-)
+library(ggplot2)
+library(reshape2)
 
 
 load_data <- function(filename) {
 	df <- read.csv(paste("resources/", filename, sep=""),
                header = TRUE,
                quote="\"",
-#               stringsAsFactors= TRUE,
+            #   stringsAsFactors= TRUE,
                strip.white = TRUE);
     return(df)
 }
 
 
 filter_site_id <- function(site_id, data) {
-	df <- data[data$site_os_id == site_id, ];
+	df <- data[data$Hospital_ID == site_id, ];
 	return(df)
 }
 
@@ -37,81 +25,39 @@ filter_outcome <- function(outcome_id, data) {
 	return(df)
 }
 
-create_bar_plot <- function(title, data) {
-    
 
-    if (nrow(data[data$race == "Black", ]) > 0) {
-        black <- c(data[data$race == "Black", "Percent_State"], 
-                   data[data$race == "Black", "Percent_Region"], 
-                   data[data$race == "Black", "Percent_Hospital"]);
-    } else {
-        black <- c(0,0,0)
-    }
-    
-    if (nrow(data[data$race == "White", ]) > 0) {
-        white <- c(data[data$race == "White", "Percent_State"], 
-                   data[data$race == "White", "Percent_Region"], 
-                   data[data$race == "White", "Percent_Hospital"]);
-    } else {
-        white <- c(0,0,0)
-    }
-    
-    if (nrow(data[data$race == "FL_Hispanic", ]) > 0) {
-        flhp <- c(data[data$race == "FL_Hispanic", "Percent_State"], 
-                  data[data$race == "FL_Hispanic", "Percent_Region"],  
-                  data[data$race == "FL_Hispanic", "Percent_Hospital"]);
-    } else {
-        flhp <- c(0,0,0)
-    }
-    
-    if (nrow(data[data$race == "PR_Hispanic", ]) > 0) {
-        prhp <- c(data[data$race == "PR_Hispanic", "Percent_State"], 
-                  data[data$race == "PR_Hispanic", "Percent_Region"],  
-                  data[data$race == "PR_Hispanic", "Percent_Hospital"]);
-    } else {
-        prhp <- c(0,0,0)
-    }
-    
-    
-    black <- black * 100;
-    white <- white * 100;
-    flhp <- flhp * 100;
-    prhp <- prhp * 100;
-
-    bplt <- barplot(
-        cbind(black, white, flhp, prhp),
-        beside=TRUE,
-        horiz=TRUE,
-        xlim=c(0,100),
-        legend.text=c("Percent State", "Percent Region", "Percent Hospital"),
-        names.arg=c("Black", "White", "FL Hispanic", "PR Hispanic"),
-        main=title,
-        col=c("red", "blue", "orange"),
-        xpd=TRUE
-    )
-
-    grid(ny=NA, nx=NULL)
-
+create_plot <- function(outcome, df) {
+	df <- df_restructure(df);
+    g <- ggplot(data=df, aes(x=race, y=value, fill=variable)) + geom_bar(stat="identity", position=position_dodge(), ) + coord_flip();
+    g <- g + theme(legend.position="bottom", legend.title=element_blank(), plot.margin=unit(c(2,2,2,2), "cm")) + ggtitle(outcome) + ylab("") + xlab("") + scale_y_continuous(breaks=seq(0, 1, 0.1));
+    g <- g + geom_text(aes(label=value), position=position_dodge(width=0.9), hjust=1.5);
+    print(g);
 
 }
 
+df_restructure <- function(df) {
+    df <- melt(df, id=c("race", "Hospital_ID", "outcome", "Region"));
+    return(df);
+}
 
 full_data <- load_data('data.csv');
-# site_id_vector <- unique(full_data$site_os_id);
-
-site_id_vector = c(13710);
+site_id_vector <- unique(full_data$Hospital_ID);
 
 
 for (site_id in site_id_vector) {
+    
+    print(paste("Creating pdf for site ", site_id));
     
     filename <- paste("reports/", site_id, ".pdf", sep="");
     pdf(file = filename);
     
     df1 <- filter_site_id(site_id, full_data);
     
-    for (outcome in 1:11){
+    outcomes_vector <- unique(df1$outcome);
+    
+    for (outcome in outcomes_vector){
         df2 <- filter_outcome(outcome, df1);
-        create_bar_plot(outcomes_vector[outcome], df2);
+        create_plot(outcome, df2);
     }
     
     dev.off();
