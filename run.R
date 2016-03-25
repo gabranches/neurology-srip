974e0b6019793f53f41c1814103b35b44e2ff854
Gustavo Abranches <gabranches@MacBook-Pro-3.local>
2016-03-25 17:00:31 -0400

    added front page and stroke stats

--- run.R
+++ run.R
@@ -1,5 +1,6 @@
 setwd("/Users/gabranches/GoogleDrive/work/R/srip")
 
+require(png)
 require(ggplot2)
 require(reshape2)
 
@@ -67,6 +68,7 @@ create_ethnicities_chart <- function(outcome, df) {
     
     g <- ggplot(data=df, aes(x=race, y=value, fill=variable)) +
         geom_bar(size=.3, colour="black", stat="identity", position=position_dodge(), ) + coord_flip() +
+        theme_grey() +
         theme(legend.position="bottom", legend.title=element_blank(), plot.margin=unit(c(2,2,2,2), "cm")) +
         ggtitle(outcome) + ylab("") + xlab("") +
         scale_y_continuous(labels=percent, limits=c(0,1), breaks=seq(0, 1, 0.1)) +
@@ -80,6 +82,7 @@ create_gender_chart <- function(outcome, df) {
     
     g <- ggplot(data=df, aes(x=sex, y=value, fill=variable)) +
         geom_bar(size=.3, colour="black", stat="identity", position=position_dodge(), ) + coord_flip() +
+        theme_grey() +
         theme(legend.position="bottom", legend.title=element_blank(), plot.margin=unit(c(2,2,2,2), "cm")) +
         ggtitle(outcome) + ylab("") + xlab("") +
         scale_y_continuous(labels=percent, limits=c(0,1), breaks=seq(0, 1, 0.1)) +
@@ -91,24 +94,37 @@ create_gender_chart <- function(outcome, df) {
 
 create_rank_graph <- function(site_id) {
     
-    g <- ggplot(data=rank_data, aes(reorder(x=factor(Hospital_ID), DFC_PERCENT), y=DFC_PERCENT)) +
+    g <- ggplot(data=dfc_rank_data, aes(reorder(x=factor(Hospital_ID), DFC_PERCENT), y=DFC_PERCENT)) +
         geom_bar(stat="identity", aes(fill = Hospital_ID == site_id )) + 
-        theme(plot.margin=unit(c(.5,.5,-3,0), "cm"), legend.position="none", axis.text.x = element_blank(), axis.ticks.x=element_blank()) +
+        theme_grey() +
+        theme(plot.margin=unit(c(.5,.5,-2,0), "cm"), legend.position="none", axis.text.x = element_blank(), axis.ticks.x=element_blank()) +
         scale_y_continuous(labels=percent, limits=c(0,1), breaks=seq(0, 1, 0.1)) +
         scale_fill_manual(values = c("lightblue", "red")) + 
         geom_text(aes(label=DFC_PERCENT*100), size=2, position=position_dodge(width=0.9), hjust=-.3, angle=90) +
         ggtitle("Defect Free Care Performance") + ylab("") + xlab("");
 
-    summary_text(g);
+    summary_text(g, site_id);
 
 }
 
-summary_text <- function(p) {
-    text <- paste("The following is text that'll appear in a plot window.\n",
-         "       As you can see, it's in the plot window\n",
-         "       One might imagine useful informaiton here")
+summary_text <- function(p, site_id) {
+    
+    df_stats <-  dfc_stats_data[dfc_stats_data$id == site_id, ];
+    strokes_site <- sum(df_stats$Black) + 
+        sum(df_stats$FL_Hispanic) + 
+        sum(df_stats$PR_Hispanic) + 
+        sum(df_stats$White);
+    
+    text <- paste("Hospital Demographics:\n",
+                    "Hospital ID", site_id, "Total Ischemic Strokes:\n",
+                    strokes_site, "of", total_ischemic_strokes, "Total Ischemic Strokes in the FL-PR Stroke Registry\n\n",
+                    "Hospital ID", site_id, "Strokes by Race-Ethnicity:\n",
+                    "Non-Hispanic Black:", df_stats$Black, "(", round(df_stats$Black/strokes_site*100, digits=0), "%)\n",
+                    "Non-Hispanic White:", df_stats$White, "(", round(df_stats$White/strokes_site*100, digits=0), "%)\n",
+                    "Hispanic:", df_stats$FL_Hispanic+df_stats$PR_Hispanic, "(", round(df_stats$FL_Hispanic+df_stats$PR_Hispanic/strokes_site*100, digits=0), "%)");
+    
     g <- ggplot() + 
-        annotate("text", x = 4, y = 10, size=6, label = text) + 
+        annotate("text", x = 4, y = 8, size=5, label = text) + 
         theme_bw() +
         theme(axis.line=element_blank(),axis.text.x=element_blank(),
           axis.text.y=element_blank(),axis.ticks=element_blank(),
@@ -116,8 +132,28 @@ summary_text <- function(p) {
           axis.title.y=element_blank(),legend.position="none",
           panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
           panel.grid.minor=element_blank(),plot.background=element_blank());
+         
         
     multiplot(p, g);
+}
+
+front_page <- function(site_id) {
+    
+    
+    text <- paste("FL-PR CReSD Disparities Dashboard: \n", "2014\n",
+                    "Hospital ID:", site_id);
+    
+    g <- ggplot() + 
+        annotate("text", x = 4, y = 8, size=5, label = text) + 
+        theme_bw() +
+        theme(axis.line=element_blank(),axis.text.x=element_blank(),
+          axis.text.y=element_blank(),axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),legend.position="none",
+          panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),plot.background=element_blank());
+          
+    print(g);
 }
 
 
@@ -136,6 +172,7 @@ create_charts <- function() {
         filename <- paste("reports/", site_id, ".pdf", sep="");
         pdf(file = filename);
         
+        front_page(site_id);
         create_rank_graph(site_id);
         
         df1_eth <- filter_site_id(site_id, ethnicity_data);
@@ -163,12 +200,19 @@ create_charts <- function() {
 # Run
 print("Running...");
 ethnicity_data <- load_data('ethnicity.csv');
-rank_data <- load_data('dfc_ranks.csv');
+dfc_rank_data <- load_data('dfc_ranks.csv');
+dfc_stats_data <- load_data('dfc_stats.csv');
 gender_data <- load_data('gender.csv');
 
-# create_charts();   
+logo_img <- readPNG("resources/logo.png");
 
-create_rank_graph(10140);
+total_ischemic_strokes <- sum(dfc_stats_data$Black) + 
+    sum(dfc_stats_data$FL_Hispanic) + 
+    sum(dfc_stats_data$PR_Hispanic) + 
+    sum(dfc_stats_data$White);
+    
+create_charts();   
+
 print("Done.");
 
 
